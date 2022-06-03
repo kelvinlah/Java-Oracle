aadea903c7e0fcd249e8c1f2647ad24c78a04f72
Kelvin <klvn2003@uvers.ac.id>
2022-06-03 15:24:06 +0700

    Menghapus beberapa file

--- Section 2/Practice/ProblemSet2_2/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
+++ Section 2/Practice/ProblemSet2_2/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -1,14 +1,16 @@
-package text01;
+package problemset2_2;
 
-public class Text01 {
+public class ProblemSet2_2 {
     public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
         System.out.println("   /\\         /\\   ");
         System.out.println("  /  \\_______/  \\  ");
         System.out.println(" /               \\ ");
         System.out.println("(  /\\         /\\  )");
         System.out.println("====     V     ====");
-        System.out.println("======(__|__)======");
-        System.out.println("  (             )  ");
+        System.out.println("======  _|_ ======");
+        System.out.println("  (    /   \\    )  ");
         System.out.println("   (___________)   ");
-    }    
+    } 
 }
